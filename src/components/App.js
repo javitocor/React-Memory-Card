9a9e957e7598ffe1df482f105ed7cb381e3dca23
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-06 10:22:45 +0200

    create components files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
+import Header from './Header';
+import Display from './Display';
 
-function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
+
+  const incrementBestScore = () => {
+    setBestScore(bestScore + 1);
+  };
+
   return (
     <div className="App">
-     
+      <Header score={score} bestScore={bestScore}/>
+      <Display />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (props) => {
+  const {score, bestScore} = props;
+  
+  return (
+
+  );
+}
+
+export default Header;
