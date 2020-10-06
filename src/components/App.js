14a9c66facc272c35847a30b6a23dfd6214a664d
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-06 15:09:07 +0200

    add handle click

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from './Header';
 import Display from './Display';
 
 const App = () => {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
+  const [clicked, setClicked] = useState([]);
+
+  useEffect(() => {
+    if(score >= bestScore) {
+      incrementBestScore();
+    }
+  })
 
   const incrementScore = () => {
     setScore(score + 1);
   };
 
   const incrementBestScore = () => {
-    setBestScore(bestScore + 1);
+    setBestScore(score);
+  };
+
+  const check = () => {
+    if(score === 12) {
+      return true;
+    }
+    return false;
+  };
+
+  const memory = (id) =>{
+    if(clicked.includes(id) === false) {
+      incrementScore();
+      setClicked([...clicked, id]);      
+    } else {
+      setScore(0);
+      setClicked([]);
+    }
   };
 
   return (
     <div className="App">
-      <Header score={score} bestScore={bestScore}/>
-      <Display />
+      {check() ? (<div><h1>Congratulations!!!</h1></div>) :
+      (<div><Header score={score} bestScore={bestScore}/>
+      <Display memory={memory} score={score}/></div>)
+    }
     </div>
   );
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import '../style/Display.css';
 import data from '../data';
+import shuffle from '../shuffle';
 
 const Display = (props) => {
+  const [cards, setCards] = useState(() => shuffle(data));
+  const {memory, score} = props;
+
+  useEffect(() => {
+    setCards(shuffle(data));    
+  }, [score]);
+
+  const handleClick = (e, id) =>{
+    memory(id);
+  }
 
   return (
     <div className="container-fluid">
@@ -11,13 +22,13 @@ const Display = (props) => {
       </header>
       <main className="jumbotron jumbotron-fluid">
         <div className="row d-flex justify-content-between mx-2">
-          {data.map(item=>{
-            return <div className="col-3"><div class="card mb-2">
-                <img class="card-img-top" src={item.image} alt="player pic"/>
-                <div class="card-body">
-                  <ul class="list-group list-group-flush">
-                    <li class="list-group-item">{item.name}</li>
-                    <li class="list-group-item">{item.nationality}</li>
+          {cards.map(item=>{
+            return <div className="col-3"><div className="card mb-2" id={item.id} onClick={(e)=>handleClick(e, item.id)}>
+                <img className="card-img-top" src={item.image} alt="player pic"/>
+                <div className="card-body">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">{item.name}</li>
+                    <li className="list-group-item">{item.nationality}</li>
                   </ul>
                 </div>
               </div>
