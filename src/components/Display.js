4751d8c057228823be98d1f71369cd51a6a05da2
javitocor <56809963+javitocor@users.noreply.github.com>
2020-10-06 12:01:00 +0200

    add styling

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,15 +1,29 @@
 import React from "react";
+import '../style/Display.css';
 import data from '../data';
 
 const Display = (props) => {
 
   return (
-    <div className="jumbotron jumbotron-fluid">
-      <header>
+    <div className="container-fluid">
+      <header className="bg-dark text-center  text-white p-3 mt-2">
         <h3>Get points by clicking on an image but don't click on any more than once!</h3>
       </header>
-      <main>
-
+      <main className="jumbotron jumbotron-fluid">
+        <div className="row d-flex justify-content-between mx-2">
+          {data.map(item=>{
+            return <div className="col-3"><div class="card mb-2">
+                <img class="card-img-top" src={item.image} alt="player pic"/>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">{item.name}</li>
+                    <li class="list-group-item">{item.nationality}</li>
+                  </ul>
+                </div>
+              </div>
+              </div>
+          })}
+        </div>
       </main>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const Header = (props) => {
   return (
     <header className="header bg-success mb-1 text-dark d-flex justify-content-between">
       <div>
-        <h2></h2>
+        <h2 className="p-2">Snooker Memory Game</h2>
       </div>
-      <div>
-        <ul className="list-inline">
+      <div className="p-2 mr-3">
+        <ul className="list-inline font-weight-bolder">
           <li className="list-inline-item">Score: {score}</li>
           <li className="list-inline-item">Best Score: {bestScore}</li>
         </ul>
